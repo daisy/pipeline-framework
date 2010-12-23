9448e3038d40936fb4f355bc963cb3b2b8909821
Romain Deltour <rdeltour@gmail.com>
2010-12-23 13:22:18 +0100

    fixed *.bat script file

--- dist/pipeline2.bat
+++ dist/pipeline2.bat
@@ -5,7 +5,7 @@ set JAVA=java
 set CMD=%~n0
 set MODULE_DIR=%~dp0
 
-set CP=%MODULE_DIR%\lib\bndtools.launcher-0.0.0.20100513-0222.jar;%MODULE_DIR%\lib\org.eclipse.osgi-3.6.1.jar
+set CP="%MODULE_DIR%\lib\bndtools.launcher-0.0.0.20100513-0222.jar";"%MODULE_DIR%\lib\org.eclipse.osgi-3.6.1.jar"
 
 rem echo %CP%
 %JAVA%  -Dorg.daisy.pipeline.cmdargs="%*" -cp %CP%  bndtools.launcher.Main launch.properties
