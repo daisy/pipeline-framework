6c3c31c9cf526118f4a9d06bd4f6b1f2ba9a5541
Romain Deltour <rdeltour@gmail.com>
2011-06-28 13:18:11 +0200

    webservice: it is now started in a ServiceTrackerCustomizer, to wait for the DaisyPipelineContext

--- webservice/src/org/daisy/pipeline/webservice/Activator.java
+++ webservice/src/org/daisy/pipeline/webservice/Activator.java
@@ -4,32 +4,67 @@ import org.daisy.pipeline.DaisyPipelineContext;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 import org.restlet.Component;
 import org.restlet.data.Protocol;
 
 public class Activator implements BundleActivator {
 
+	private class PipelineContextTrackerCustomizer implements ServiceTrackerCustomizer{
+
+		@Override
+		public Object addingService(ServiceReference ref) {
+			DaisyPipelineContext daisyPipelineContext = (DaisyPipelineContext) context.getService(ref);
+			Component component = new Component();
+			component.getServers().add(Protocol.HTTP, 8182);
+			WebApplication application = new WebApplication();
+			application.setDaisyPipelineContext(daisyPipelineContext);
+		
+			component.getDefaultHost().attach("/ws", application);
+			
+			// TODO: how to get this information dynamically?
+			application.setServerAddress("http://localhost:8182/ws");
+			
+			try {
+				component.start();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void modifiedService(ServiceReference arg0, Object arg1) {
+		}
+
+		@Override
+		public void removedService(ServiceReference arg0, Object arg1) {
+			//TODO stop the component
+		}
+		
+	}
+	private ServiceTracker pipelineContextTracker;
+private BundleContext context;
+	
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
+		this.context=context;
 		System.out.println("Starting webservice on port 8182.");
 		
-		ServiceReference sr = context.getServiceReference(org.daisy.pipeline.DaisyPipelineContext.class.getName());
-		DaisyPipelineContext daisyPipelineContext = (DaisyPipelineContext)context.getService(sr);
+		pipelineContextTracker = new ServiceTracker(context, DaisyPipelineContext.class.getName(), new PipelineContextTrackerCustomizer());
+		pipelineContextTracker.open();
 		
-		Component component = new Component();
-		component.getServers().add(Protocol.HTTP, 8182);
-		WebApplication application = new WebApplication();
-		application.setDaisyPipelineContext(daisyPipelineContext);
-	
-		component.getDefaultHost().attach("/ws", application);
+		DaisyPipelineContext daisyPipelineContext = (DaisyPipelineContext) pipelineContextTracker.waitForService(5000);
+//		ServiceReference sr = context.getServiceReference(org.daisy.pipeline.DaisyPipelineContext.class.getName());
+//		DaisyPipelineContext daisyPipelineContext = (DaisyPipelineContext)context.getService(sr);
 		
-		// TODO: how to get this information dynamically?
-		application.setServerAddress("http://localhost:8182/ws");
 		
-		component.start();
 	}
 	
 	/*
@@ -37,6 +72,7 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
+		pipelineContextTracker.close();
 		System.out.println("Webservice stopped.");
 	}
 
