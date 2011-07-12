544e12b821392f60886787947ffe512390edda00
capitan cambio <capitan.cambio@gmail.com>
2011-07-12 17:45:17 +0100

    now the bin folder is called fbin from felix binary

--- Main/dp2.sh
+++ Main/dp2.sh
@@ -6,7 +6,7 @@ ARGS="$(echo ${@})"
 DP_ARGS=-Dorg.daisy.pipeline.cmdargs=\"$ARGS\"
 FELIX_CONF="file:$PWD/etc/config.properties"
 XPROC_CONF=$PWD/etc/conf_calabash.xml
-$JAVA $DS_MODE -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -Dorg.daisy.pipeline.cmdargs="$ARGS" -jar bin/felix.jar
+$JAVA $DS_MODE -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -Dorg.daisy.pipeline.cmdargs="$ARGS" -jar fbin/felix.jar
 
 
 
--- Main/dp2ws.sh
+++ Main/dp2ws.sh
@@ -3,7 +3,7 @@ JAVA=java
 DS_MODE="-Dorg.daisy.pipeline.mode=ws"
 FELIX_CONF="file:$PWD/etc/config.properties"
 XPROC_CONF=$PWD/etc/conf_calabash.xml
-$JAVA $DS_MODE -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -Dorg.daisy.pipeline.cmdargs="$ARGS" -jar bin/felix.jar
+$JAVA $DS_MODE -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -Dorg.daisy.pipeline.cmdargs="$ARGS" -jar fbin/felix.jar
 
 
 
