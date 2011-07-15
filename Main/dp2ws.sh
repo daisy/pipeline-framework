d75569fdd4af37c9087e0e11855464c1389f62b7
Romain Deltour <rdeltour@gmail.com>
2011-07-15 13:30:48 +0200

    cleaned the bash scripts

--- Main/dp2ws.sh
+++ Main/dp2ws.sh
@@ -5,7 +5,8 @@ FELIX_CONF="file:$PWD/etc/config.properties"
 DP_LOG_DIR=$PWD/log
 XPROC_CONF=$PWD/etc/conf_calabash.xml
 LOGBACK_CONF=etc/conf_logback.xml
-$JAVA $DS_MODE -Dorg.daisy.pipeline.logdir="$DP_LOG_DIR" -Dlogback.configurationFile="$LOGBACK_CONF" -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -Dorg.daisy.pipeline.cmdargs="$ARGS" -jar fbin/felix.jar
+
+$JAVA $DS_MODE -Dorg.daisy.pipeline.logdir="$DP_LOG_DIR" -Dlogback.configurationFile="$LOGBACK_CONF" -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -jar fbin/felix.jar
 
 
 
