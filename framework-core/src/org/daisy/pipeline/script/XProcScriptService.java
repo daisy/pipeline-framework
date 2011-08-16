fa4c449a8c205531945f46c9cf931c49cab75541
Romain Deltour <rdeltour@gmail.com>
2011-08-16 22:01:23 +0200

    Modified the ScriptRegisty:

---
+++ framework-core/src/org/daisy/pipeline/script/XProcScriptService.java
@@ -0,0 +1,71 @@
+package org.daisy.pipeline.script;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+public final class XProcScriptService {
+
+	/** The Constant CONVERTER_URL. */
+	public static final String CONVERTER_URL = "converter.url";
+
+	/** The Constant CONVERTER_DESCRIPTION. */
+	public static final String CONVERTER_DESCRIPTION = "converter.description";
+
+	/** The Constant CONVERTER_NAME. */
+	public static final String CONVERTER_NAME = "converter.name";
+
+	private URI uri;
+	private String name;
+	private String description;
+
+	public XProcScriptService() {
+	}
+
+	public void activate(Map<?, ?> properties) {
+		if (properties.get(CONVERTER_NAME) == null
+				|| properties.get(CONVERTER_NAME).toString().isEmpty()) {
+			throw new IllegalArgumentException(CONVERTER_NAME
+					+ " property must not be empty");
+		}
+
+		if (properties.get(CONVERTER_DESCRIPTION) == null
+				|| properties.get(CONVERTER_DESCRIPTION).toString().isEmpty()) {
+			throw new IllegalArgumentException(CONVERTER_DESCRIPTION
+					+ " property must not be empty");
+		}
+		if (properties.get(CONVERTER_URL) == null
+				|| properties.get(CONVERTER_URL).toString().isEmpty()) {
+			throw new IllegalArgumentException(CONVERTER_URL
+					+ " property must not be empty");
+		}
+		try {
+			this.uri = new URI(properties.get(CONVERTER_URL).toString());
+		} catch (URISyntaxException e) {
+			throw new IllegalArgumentException(CONVERTER_URL
+					+ " property must not be a legal URI");
+		}
+		this.name = properties.get(CONVERTER_NAME).toString();
+		this.description = properties.get(CONVERTER_DESCRIPTION).toString();
+	}
+
+	public URI getURI() {
+		return uri;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String toString() {
+		StringBuffer buf = new StringBuffer();
+		buf.append("Name: " + name);
+		buf.append(", desc: " + description);
+		buf.append(", uri: " + uri.toString());
+		return buf.toString();
+	}
+}
