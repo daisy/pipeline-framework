19dda4fc4510f15c665792c5a781821953a0fe6b
Romain Deltour <rdeltour@gmail.com>
2011-08-24 18:53:47 +0200

    minor refactoring

--- org.daisy.pipeline.logging/src/org/daisy/pipeline/logging/Activator.java
+++ org.daisy.pipeline.logging/src/org/daisy/pipeline/logging/Activator.java
@@ -10,32 +10,21 @@ import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
-	private static final Logger mLogger = LoggerFactory.getLogger(Activator.class);
-	static BundleContext getContext() {
-		return context;
-	}
+	private static final Logger mLogger = LoggerFactory
+			.getLogger(Activator.class);
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext bundleContext) throws Exception {
-		mLogger.info( "earlyStartup slf4j SLF4JBridgeHandler...");
-		java.util.logging.Logger rootLogger = LogManager.getLogManager().getLogger("");  
-		Handler[] handlers = rootLogger.getHandlers();  
-		for (int i = 0; i < handlers.length; i++) {  
-		    rootLogger.removeHandler(handlers[i]);  
-		} 
+		mLogger.info("earlyStartup slf4j SLF4JBridgeHandler...");
+		java.util.logging.Logger rootLogger = LogManager.getLogManager()
+				.getLogger("");
+		Handler[] handlers = rootLogger.getHandlers();
+		for (int i = 0; i < handlers.length; i++) {
+			rootLogger.removeHandler(handlers[i]);
+		}
 		org.slf4j.bridge.SLF4JBridgeHandler.install();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
 	}
 
 }
