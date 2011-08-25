bbb00e27916ad1bcdc8a7f79aa1a60351b6f18cd
Romain Deltour <rdeltour@gmail.com>
2011-08-25 03:36:19 +0200

    slightly refactored the module registry: moved the tracker inside the registry

--- module-registry/src/main/java/org/daisy/pipeline/modules/tracker/DefaultModuleRegistry.java
+++ module-registry/src/main/java/org/daisy/pipeline/modules/tracker/DefaultModuleRegistry.java
@@ -1,51 +1,115 @@
 package org.daisy.pipeline.modules.tracker;
 
 import java.net.URI;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 
+import org.daisy.expath.parser.DefaultModuleBuilder;
 import org.daisy.expath.parser.EXPathPackageParser;
 import org.daisy.pipeline.modules.Component;
 import org.daisy.pipeline.modules.Module;
 import org.daisy.pipeline.modules.ModuleRegistry;
+import org.daisy.pipeline.modules.ResourceLoader;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
+import org.osgi.util.tracker.BundleTracker;
+import org.osgi.util.tracker.BundleTrackerCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DefaultModuleRegistry implements ModuleRegistry {
 
-	HashMap<URI, Module> mComponentsMap= new HashMap<URI, Module>();
-	HashSet<Module> mModules= new HashSet<Module>();
-	private EXPathPackageParser mParser;
-	private Logger mLogger = LoggerFactory.getLogger(getClass());
+	private final class ModuleTracker implements BundleTrackerCustomizer {
+		@Override
+		public Object addingBundle(final Bundle bundle,
+				final BundleEvent event) {
+			Bundle result = null;
+			URL url = bundle.getResource("expath-pkg.xml");
+			if (url != null) {
+				logger.trace("tracking '{}' <{}>",
+						bundle.getSymbolicName(), url);
 
-	/*
-	private final Function<Bundle, Module> toModule = new Function<Bundle, Module>() {
+				Module module = mParser.parse(url,
+						new DefaultModuleBuilder()
+								.withLoader(new ResourceLoader() {
 
-		public Module apply(Bundle bundle) {
-		
-			return parser.parse(bundle.getEntry("expath-pkg.xml"));
+									public URL loadResource(
+											String path) {
+
+										// TODO: this is not
+										// efficient at all, assure
+										// to
+										// load the whole path
+										// while loading the bundle
+										// Enumeration res =
+										// bundle.findEntries("/",
+										// path,
+										// true);
+										// if(res==null)
+										// return null;
+										// String completePath =
+										// res.nextElement().toString();
+										URL url = bundle
+												.getResource(path);
+										return url;
+									}
+								}));
+
+				// System.out.println(module.getName());
+				addModule(module);
+				result = bundle;
+
+			}
+
+			// Finally
+			return result;
 		}
 
-	};
-	 */
-	private EXPathPackageTracker tracker;
-	
+		@Override
+		public void modifiedBundle(Bundle bundle,
+				BundleEvent event, Object object) {
+			// TODO reset module
+		}
+
+		@Override
+		public void removedBundle(Bundle bundle, BundleEvent event,
+				Object object) {
+			logger.trace("removing bundle '{}' [{}] ",
+					bundle.getSymbolicName(), event);
+			// FIXME remove module
+			// bundles.remove(bundle.getSymbolicName());
+		}
+	}
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(DefaultModuleRegistry.class);
+
+	private HashMap<URI, Module> componentsMap = new HashMap<URI, Module>();
+	private HashSet<Module> modules = new HashSet<Module>();
+	private EXPathPackageParser mParser;
+
+	private BundleTracker tracker;
 
 	public DefaultModuleRegistry() {
 	}
 
 	public void init(BundleContext context) {
-		tracker = new EXPathPackageTracker(context,this);
-		tracker.setParser(mParser);
+		logger.trace("Initializing module registry");
+		tracker = new BundleTracker(context, Bundle.ACTIVE,
+				new ModuleTracker());
 		tracker.open();
-		mLogger.debug("Module registry up");
-
-		
+		//TODO open the tracker in a separate thread ?
+		// new Thread() {
+		// @Override
+		// public void run() {
+		// tracker.open();
+		// }
+		// }.start();
+		logger.trace("Module registry up");
 	}
-	
-	
 
 	public void close() {
 		tracker.close();
@@ -56,11 +120,11 @@ public class DefaultModuleRegistry implements ModuleRegistry {
 	}
 
 	public Iterator<Module> iterator() {
-		return mModules.iterator();
+		return modules.iterator();
 	}
 
 	public Module getModuleByComponent(URI uri) {
-		return mComponentsMap.get(uri);
+		return componentsMap.get(uri);
 	}
 
 	public Module resolveDependency(URI component, Module source) {
@@ -70,16 +134,16 @@ public class DefaultModuleRegistry implements ModuleRegistry {
 
 	@Override
 	public Iterable<URI> getComponents() {
-		return mComponentsMap.keySet();
+		return componentsMap.keySet();
 	}
 
 	@Override
 	public void addModule(Module module) {
-		mModules.add(module);
-		mLogger.debug("Registring "+module.getName());
-		for(Component component: module.getComponents()){
-			mLogger.debug("Component "+component.getURI());
-			mComponentsMap.put(component.getURI(), module);
-		}		
+		logger.debug("Registring module {}", module.getName());
+		modules.add(module);
+		for (Component component : module.getComponents()) {
+			logger.debug("  - {}", component.getURI());
+			componentsMap.put(component.getURI(), module);
+		}
 	}
 }
