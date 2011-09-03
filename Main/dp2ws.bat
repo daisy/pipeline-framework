2f37048181f17950f5288817c9048012acd93f0f
Romain Deltour <rdeltour@gmail.com>
2011-09-03 03:19:19 +0200

    updated the WS launchers with the new IO dir property

--- Main/dp2ws.bat
+++ Main/dp2ws.bat
@@ -5,5 +5,6 @@ set DP_LOG_DIR=%cd%/log
 set FELIX_CONF="file:/%cd%/etc/config.properties"
 set XPROC_CONF="file:/%cd:\=/%/etc/conf_calabash.xml"
 set LOGBACK_CONF=etc/conf_logback.xml
-%JAVA% %DS_MODE% -Dorg.daisy.pipeline.logdir="%DP_LOG_DIR%" -Dlogback.configurationFile="%LOGBACK_CONF%" -Dfelix.config.properties="%FELIX_CONF%" -Dorg.daisy.pipeline.xproc.configuration="%XPROC_CONF%" -jar fbin/felix.jar
+set TMP_IO_DIR=%cd%/data
+%JAVA% %DS_MODE% -Dorg.daisy.pipeline.iobase="%TMP_IO_DIR%" -Dorg.daisy.pipeline.logdir="%DP_LOG_DIR%" -Dlogback.configurationFile="%LOGBACK_CONF%" -Dfelix.config.properties="%FELIX_CONF%" -Dorg.daisy.pipeline.xproc.configuration="%XPROC_CONF%" -jar fbin/felix.jar
 
--- Main/dp2ws.sh
+++ Main/dp2ws.sh
@@ -5,8 +5,8 @@ FELIX_CONF="file:$PWD/etc/config.properties"
 DP_LOG_DIR=$PWD/log
 XPROC_CONF=$PWD/etc/conf_calabash.xml
 LOGBACK_CONF=etc/conf_logback.xml
-
-$JAVA $DS_MODE -Dorg.daisy.pipeline.logdir="$DP_LOG_DIR" -Dlogback.configurationFile="$LOGBACK_CONF" -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -jar fbin/felix.jar
+TMP_IO_DIR=$PWD/data
+$JAVA $DS_MODE -Dorg.daisy.pipeline.iobase="$TMP_IO_DIR" -Dorg.daisy.pipeline.logdir="$DP_LOG_DIR" -Dlogback.configurationFile="$LOGBACK_CONF" -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -jar fbin/felix.jar
 
 
 
