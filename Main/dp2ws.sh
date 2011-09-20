3f7f79235d9d3c5d2ba0589df1bd506f3629eb1b
capitan cambio <capitan.cambio@gmail.com>
2011-09-20 17:15:17 +0100

    adding org.daisy.pipeline.iobase to the dp2ws.sh script

--- Main/dp2ws.sh
+++ Main/dp2ws.sh
@@ -6,7 +6,8 @@ DP_LOG_DIR=$PWD/log
 XPROC_CONF=$PWD/etc/conf_calabash.xml
 LOGBACK_CONF=etc/conf_logback.xml
 TMP_IO_DIR=$PWD/data
-$JAVA $DS_MODE -Dorg.daisy.pipeline.iobase="$TMP_IO_DIR" -Dorg.daisy.pipeline.logdir="$DP_LOG_DIR" -Dlogback.configurationFile="$LOGBACK_CONF" -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -jar fbin/felix.jar
+IO_BASE="/tmp/dp2"
+$JAVA $DS_MODE -Dorg.daisy.pipeline.iobase="$TMP_IO_DIR" -Dorg.daisy.pipeline.logdir="$DP_LOG_DIR" -Dlogback.configurationFile="$LOGBACK_CONF" -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -Dorg.daisy.pipeline.iobase="$IO_BASE" -jar fbin/felix.jar
 
 
 
