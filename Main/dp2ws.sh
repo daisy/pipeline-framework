626d5b1ffb594cc31ab8d7cd6b0441a516b16192
Romain Deltour <rdeltour@gmail.com>
2011-09-24 00:18:53 +0200

    reverted rev 17875a51a594 (IO base was already set)

--- Main/dp2ws.sh
+++ Main/dp2ws.sh
@@ -6,8 +6,7 @@ DP_LOG_DIR=$PWD/log
 XPROC_CONF=$PWD/etc/conf_calabash.xml
 LOGBACK_CONF=etc/conf_logback.xml
 TMP_IO_DIR=$PWD/data
-IO_BASE="/tmp/dp2"
-$JAVA $DS_MODE -Dorg.daisy.pipeline.iobase="$TMP_IO_DIR" -Dorg.daisy.pipeline.logdir="$DP_LOG_DIR" -Dlogback.configurationFile="$LOGBACK_CONF" -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -Dorg.daisy.pipeline.iobase="$IO_BASE" -jar fbin/felix.jar
+$JAVA $DS_MODE -Dorg.daisy.pipeline.iobase="$TMP_IO_DIR" -Dorg.daisy.pipeline.logdir="$DP_LOG_DIR" -Dlogback.configurationFile="$LOGBACK_CONF" -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -jar fbin/felix.jar
 
 
 
