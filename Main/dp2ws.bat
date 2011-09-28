dfbc76aa8351ce6b5996751c42e86d1f81ebb11f
capitan cambio <capitan.cambio@gmail.com>
2011-09-28 15:43:13 +0100

    problem loading xproc config solved in windows

--- Main/dp2ws.bat
+++ Main/dp2ws.bat
@@ -2,8 +2,8 @@
 set JAVA=java
 set DS_MODE="-Dorg.daisy.pipeline.mode=ws"
 set DP_LOG_DIR=%cd%/log
-set FELIX_CONF="file:/%cd%/etc/config.properties"
-set XPROC_CONF="file:/%cd:\=/%/etc/conf_calabash.xml"
+set FELIX_CONF=file:/%cd:\=/%/etc/config.properties
+set XPROC_CONF=etc/conf_calabash.xml
 set LOGBACK_CONF=etc/conf_logback.xml
 set TMP_IO_DIR=%cd%/data
 %JAVA% %DS_MODE% -Dorg.daisy.pipeline.iobase="%TMP_IO_DIR%" -Dorg.daisy.pipeline.logdir="%DP_LOG_DIR%" -Dlogback.configurationFile="%LOGBACK_CONF%" -Dfelix.config.properties="%FELIX_CONF%" -Dorg.daisy.pipeline.xproc.configuration="%XPROC_CONF%" -jar fbin/felix.jar
