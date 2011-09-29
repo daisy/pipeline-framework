e1525f2dd7daefbe4c915fdab10eec34aebd1a65
capitan cambio <capitan.cambio@gmail.com>
2011-09-29 13:10:49 +0100

    log back conf to ectDist (levels are untouched) and now the ws can be invoked from everywhere

--- Main/dp2ws.sh
+++ Main/dp2ws.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pushd $(dirname $0) > /dev/null
 JAVA=java
 DS_MODE="-Dorg.daisy.pipeline.mode=ws"
 FELIX_CONF="file:$PWD/etc/config.properties"
@@ -7,6 +8,6 @@ XPROC_CONF=$PWD/etc/conf_calabash.xml
 LOGBACK_CONF=etc/conf_logback.xml
 TMP_IO_DIR=$PWD/data
 $JAVA $DS_MODE -Dorg.daisy.pipeline.iobase="$TMP_IO_DIR" -Dorg.daisy.pipeline.logdir="$DP_LOG_DIR" -Dlogback.configurationFile="$LOGBACK_CONF" -Dfelix.config.properties="$FELIX_CONF" -Dorg.daisy.pipeline.xproc.configuration="$XPROC_CONF" -jar fbin/felix.jar
-
+popd > /dev/null
 
 
