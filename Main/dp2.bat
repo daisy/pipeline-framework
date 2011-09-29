2b17bd359398fcb28eea2e4b4ac3b64c9294754d
capitan cambio <capitan.cambio@gmail.com>
2011-09-29 12:08:36 +0100

    ws support for being called from anywhere and CLI slight changes.

--- Main/dp2.bat
+++ Main/dp2.bat
@@ -2,7 +2,7 @@
 set JAVA=java
 set DS_MODE="-Dorg.daisy.pipeline.mode=cmd"
 set DP_LOG_DIR=%cd%/log
-set FELIX_CONF="file:/%cd%/etc/config.properties"
-set XPROC_CONF="file:/%cd:\=/%/etc/conf_calabash.xml"
+set FELIX_CONF=file:/%cd%/etc/config.properties
+set XPROC_CONF=etc/conf_calabash.xml
 set LOGBACK_CONF=etc/conf_logback.xml
 %JAVA% %DS_MODE% -Dorg.daisy.pipeline.logdir="%DP_LOG_DIR%" -Dlogback.configurationFile="%LOGBACK_CONF%" -Dfelix.config.properties="%FELIX_CONF%" -Dorg.daisy.pipeline.xproc.configuration="%XPROC_CONF%" -Dorg.daisy.pipeline.cmdargs="%*" -jar fbin/felix.jar
--- Main/dp2ws.bat
+++ Main/dp2ws.bat
@@ -1,4 +1,5 @@
 @echo off
+pushd %~p0
 set JAVA=java
 set DS_MODE="-Dorg.daisy.pipeline.mode=ws"
 set DP_LOG_DIR=%cd%/log
@@ -7,4 +8,4 @@ set XPROC_CONF=etc/conf_calabash.xml
 set LOGBACK_CONF=etc/conf_logback.xml
 set TMP_IO_DIR=%cd%/data
 %JAVA% %DS_MODE% -Dorg.daisy.pipeline.iobase="%TMP_IO_DIR%" -Dorg.daisy.pipeline.logdir="%DP_LOG_DIR%" -Dlogback.configurationFile="%LOGBACK_CONF%" -Dfelix.config.properties="%FELIX_CONF%" -Dorg.daisy.pipeline.xproc.configuration="%XPROC_CONF%" -jar fbin/felix.jar
-
+popd
