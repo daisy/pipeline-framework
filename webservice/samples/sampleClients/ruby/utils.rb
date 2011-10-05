a096f683f5453d3b084f18ed4101db39c94adabd
Marisa DeMeglio <marisa.demeglio@gmail.com>
2011-10-04 18:03:27 -0700

    updated ruby client to use WS authorization

--- webservice/samples/sampleClients/ruby/utils.rb
+++ webservice/samples/sampleClients/ruby/utils.rb
@@ -1,5 +1,13 @@
 require 'nokogiri'
 require './settings.rb'
+require 'digest'
+require 'openssl'
+require 'base64'
+require 'time'
+require 'cgi'
+
+CLIENT_KEY = "clientkey"
+CLIENT_SECRET = "supersecret"
 
 def trace(string, prefix)
 	if Settings.instance.options[:trace] == true
@@ -32,4 +40,27 @@ def get_uri_from_shortname(name)
   end
 
   return nil
+end
+
+# the URI includes all parameters except hash
+def prepare_uri(uri)
+  uristring = ""
+  timestamp = Time.now.utc.iso8601
+  params = "key=#{CLIENT_KEY}&timestamp=#{timestamp}"
+  if uri.index("?") == nil
+    uristring = "#{uri}?#{params}"
+  else
+    uristring += "#{uri}&#{params}"
+  end
+  hash = generate_hash(uri)
+
+  return "#{uristring}&hash=#{hash}"
+
+end
+
+def generate_hash(data)
+  digest = OpenSSL::Digest::Digest.new('sha1')
+  hash = OpenSSL::HMAC.digest(digest, CLIENT_SECRET, data)
+  hash64 = Base64.encode64(hash).chomp
+  return CGI.escape(hash64)
 end
