fb849db651d10a65dfe3855faf481b5dfc7527ea
capitan cambio <capitan.cambio@gmail.com>
2011-11-21 10:59:21 +0000

    unit testing

---
+++ xmlcatalog.test/src/test/java/org/daisy/pipeline/xmlcatalog/stax/test/AllTest.java
@@ -0,0 +1,12 @@
+package org.daisy.pipeline.xmlcatalog.stax.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ XmlCatalogParserTest.class })
+public class AllTest {
+
+}
