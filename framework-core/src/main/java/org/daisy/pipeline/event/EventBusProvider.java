714b072aa5d76365979a5fd69c668c7c0ceb8b16
capitan cambio <capitan.cambio@gmail.com>
2012-07-02 22:08:26 +0100

    EventBus is not async any more

--- framework-core/src/main/java/org/daisy/pipeline/event/EventBusProvider.java
+++ framework-core/src/main/java/org/daisy/pipeline/event/EventBusProvider.java
@@ -1,13 +1,10 @@
 package org.daisy.pipeline.event;
 
-import java.util.concurrent.Executors;
-
 import com.google.common.base.Supplier;
-import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 
 public class EventBusProvider implements Supplier<EventBus>{
-	private final EventBus mEventBus=new AsyncEventBus(Executors.newCachedThreadPool());
+	private final EventBus mEventBus=new EventBus();//AsyncEventBus(Executors.newFixedThreadPool(10));
 
 	@Override
 	public EventBus get() {
