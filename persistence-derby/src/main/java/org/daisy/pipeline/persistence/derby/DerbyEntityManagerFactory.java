f99788a1f8e9e7cc740942fff02ed550fb4b21a7
capitan.cambio <devnull@localhost>
2012-11-21 19:30:43 +0000

    Derby publishing its properties now

--- persistence-derby/src/main/java/org/daisy/pipeline/persistence/derby/DerbyEntityManagerFactory.java
+++ persistence-derby/src/main/java/org/daisy/pipeline/persistence/derby/DerbyEntityManagerFactory.java
@@ -4,9 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.daisy.common.persistence.ForwardingEntityManagerFactory;
+
+import org.daisy.common.properties.PropertyPublisher;
+import org.daisy.common.properties.PropertyPublisherFactory;
 import org.osgi.service.jpa.EntityManagerFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 public class DerbyEntityManagerFactory extends  ForwardingEntityManagerFactory{
 
 	private static final String DERBY_JDBC_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
@@ -16,6 +20,7 @@ public class DerbyEntityManagerFactory extends  ForwardingEntityManagerFactory{
 			.getLogger(DerbyEntityManagerFactory.class.getName());
 	
 	private static  final Map<String,Object> props=new HashMap<String, Object>();
+	private PropertyPublisher propertyPublisher;
 	static{
 
 		props.put(JAVAX_PERSISTENCE_JDBC_DRIVER,
@@ -28,7 +33,16 @@ public class DerbyEntityManagerFactory extends  ForwardingEntityManagerFactory{
 	public void setBuilder(EntityManagerFactoryBuilder builder){
 		setEntityManagerFactory(builder.createEntityManagerFactory(props));
 	}
-	
+	public void setPropertyPublisherFactory(PropertyPublisherFactory propertyPublisherFactory){
+		this.propertyPublisher=propertyPublisherFactory.newPropertyPublisher();	
+		//the property publishing step goes here
+		this.propertyPublisher.publish(JAVAX_PERSISTENCE_JDBC_DRIVER,
+				DERBY_JDBC_DRIVER,this.getClass());
+		this.propertyPublisher.publish(JAVAX_PERSISTENCE_JDBC_URL,
+				DERBY_DB_URL,this.getClass());
+
+	}
+
 	public void init() {
 		logger.debug("initialize the EMF");
 		createEntityManager();
