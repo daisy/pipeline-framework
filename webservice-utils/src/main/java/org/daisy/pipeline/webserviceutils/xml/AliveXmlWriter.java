d4641300dbc6f12b5b7afdac7f7f48b346c93e86
capitan.cambio <devnull@localhost>
2012-12-07 18:25:25 +0000

    change the alive api entry definition, local attribute is now mode

--- webservice-utils/src/main/java/org/daisy/pipeline/webserviceutils/xml/AliveXmlWriter.java
+++ webservice-utils/src/main/java/org/daisy/pipeline/webserviceutils/xml/AliveXmlWriter.java
@@ -11,7 +11,7 @@ public class AliveXmlWriter {
 	public Document getXmlDocument() {
 		Document doc = XmlUtils.createDom("alive");
 		Element aliveElm = doc.getDocumentElement();
-		aliveElm.setAttribute("local", System.getProperty(Properties.LOCAL_MODE));
+		aliveElm.setAttribute("mode", System.getProperty(Properties.LOCAL_MODE,"").equalsIgnoreCase("local")+"");
 		aliveElm.setAttribute("authentication", System.getProperty(Properties.AUTHENTICATION));
 		aliveElm.setAttribute("version", System.getProperty(Properties.FRAMEWORK_VERSION));
 		if (!XmlValidator.validate(doc, XmlValidator.ALIVE_SCHEMA_URL)) {
