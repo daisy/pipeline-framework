b3d56c9a37f827ef05891b4270f8beb162cc2e86
capitan.cambio <devnull@localhost>
2013-01-22 14:24:32 +0000

    PersistentJob test update and making the unit testing of this bundle less verbose

--- framework-persistence/src/test/java/org/daisy/pipeline/persistence/jobs/PersistentJobTest.java
+++ framework-persistence/src/test/java/org/daisy/pipeline/persistence/jobs/PersistentJobTest.java
@@ -1,5 +1,10 @@
 package org.daisy.pipeline.persistence.jobs;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.daisy.pipeline.job.Job;
+import org.daisy.pipeline.job.Job;
 import org.daisy.pipeline.job.JobId;
 
 import org.daisy.pipeline.persistence.Database;
@@ -8,6 +13,8 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 public class PersistentJobTest   {
 
@@ -18,7 +25,7 @@ public class PersistentJobTest   {
 	public void setUp(){
 		//script setup
 			
-		job = new PersistentJob(Mocks.buildContext());
+		job = new PersistentJob(Job.newJob(Mocks.buildContext()),db);
 		id=job.getContext().getId();
 		db=DatabaseProvider.getDatabase();
 		db.addObject(job);
@@ -32,6 +39,25 @@ public class PersistentJobTest   {
 	public void storeTest(){
 		PersistentJob pjob= db.getEntityManager().find(PersistentJob.class,id.toString());
 		Assert.assertEquals(pjob.getContext().getId(),id);
+
+	}
+	@Test 
+	public void changeStatusTest(){
+		PersistentJob pjob= db.getEntityManager().find(PersistentJob.class,id.toString());
+		pjob.setDatabase(db);
+		pjob.changeStatus(Job.Status.DONE);
+		//System.out.println("pre udpate");
+		db.updateObject(pjob);
+	
+		//System.out.println("status post update"+pjob.getStatus());
+		pjob= db.getEntityManager().find(PersistentJob.class,id.toString());
+		Assert.assertEquals(pjob.getStatus(),Job.Status.DONE);
+
+	}
+	@Test 
+	public void getJobsTest(){
+		List<Job> jobs=PersistentJob.getAllJobs(db);
+		Assert.assertEquals(jobs.get(0).getContext().getId(),id);
 
 	}
 
