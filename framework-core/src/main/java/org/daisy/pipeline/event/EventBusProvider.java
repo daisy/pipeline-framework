b5022657e044b8ea936d5e68458b85443a18e2f6
capitancambio <capitan.cambio@gmail.com>
2013-03-14 12:55:27 +0000

    Makes eventbus async for heavy load operations (db,post status)

--- framework-core/src/main/java/org/daisy/pipeline/event/EventBusProvider.java
+++ framework-core/src/main/java/org/daisy/pipeline/event/EventBusProvider.java
@@ -1,10 +1,13 @@
 package org.daisy.pipeline.event;
 
+import java.util.concurrent.Executors;
+
 import com.google.common.base.Supplier;
+import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 
 public class EventBusProvider implements Supplier<EventBus>{
-	private final EventBus mEventBus=new EventBus();//AsyncEventBus(Executors.newFixedThreadPool(10));
+	private final EventBus mEventBus=new AsyncEventBus(Executors.newFixedThreadPool(10));
 
 	@Override
 	public EventBus get() {
