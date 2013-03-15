c531c5400136608b02cbe2a80b2f1f01f1c5b735
capitancambio <capitan.cambio@gmail.com>
2013-03-15 13:10:45 +0000

    EventBus goes directly inside the job, JobContext and all related classes updated

--- framework-core/src/main/java/org/daisy/pipeline/job/RuntimeConfigurable.java
+++ framework-core/src/main/java/org/daisy/pipeline/job/RuntimeConfigurable.java
@@ -1,10 +1,8 @@
 package org.daisy.pipeline.job;
 
-import org.daisy.pipeline.event.EventBusProvider;
 
 
 public interface RuntimeConfigurable  {
 
-	public void setEventBusProvider(EventBusProvider eventBus);
 	public void setMonitorFactory( JobMonitorFactory monitor);
 }
