695e04887a44f147b30d2d0afc6f14dcc6333221
capitancambio <capitan.cambio@gmail.com>
2013-12-04 18:26:52 +0000

    Add support to rewriteURI in the catalog parser

--- xmlcatalog/src/test/java/org/daisy/pipeline/xmlcatalog/stax/XmlCatalogParserTest.java
+++ xmlcatalog/src/test/java/org/daisy/pipeline/xmlcatalog/stax/XmlCatalogParserTest.java
@@ -42,25 +42,39 @@ public class XmlCatalogParserTest {
 	/**
 	 * Test catalog.
 	 */
-	@Test
-	public void testCatalog() {
-		Map<String, URI> publicuris=catalog.getPublicMappings();
-		Map<URI, URI> systemuris=catalog.getSystemIdMappings();
-		Map<URI, URI> uriuris=catalog.getUriMappings();
-		Assert.assertEquals(publicuris.size(), 2);
-		Assert.assertEquals(systemuris.size(), 2);
-		Assert.assertEquals(uriuris.size(), 3);
+        
+        @Test
+        public void testPublic(){
+		Map<String, URI> publicUris=catalog.getPublicMappings();
+		Assert.assertEquals(publicUris.size(), 2);
+		Assert.assertEquals(publicUris.get("-//OASIS//DTD DocBook XML V4.1.2//EN"),URI.create("mybase/docbookx.dtd"));
+		Assert.assertEquals(publicUris.get("-//OASIS//DTD DocBook MathML Module V1.0//EN"),URI.create("http://www.oasis-open.org/docbook/xml/mathml/1.0/dbmathml.dtd"));
+        }
 
-		Assert.assertEquals(publicuris.get("-//OASIS//DTD DocBook XML V4.1.2//EN"),URI.create("mybase/docbookx.dtd"));
-		Assert.assertEquals(publicuris.get("-//OASIS//DTD DocBook MathML Module V1.0//EN"),URI.create("http://www.oasis-open.org/docbook/xml/mathml/1.0/dbmathml.dtd"));
+        @Test
+        public void testSystem(){
+		Map<URI, URI> sytemUris=catalog.getSystemIdMappings();
 
-		Assert.assertEquals(systemuris.get(URI.create("http://example.com/groupfile.xml")), URI.create("mybase/mysistemIdgroup.xml"));
-		Assert.assertEquals(systemuris.get(URI.create("http://example.com/file.xml")), URI.create("mysistemId.xml"));
+		Assert.assertEquals(sytemUris.size(), 2);
 
-		Assert.assertEquals(uriuris.get(URI.create("http://example.com/groupuri.xml")), URI.create("mybase/myurigroup.xml"));
-		Assert.assertEquals(uriuris.get(URI.create("http://example.com/groupuri2.xml")), URI.create("mybase/myurigroup2.xml"));
-		Assert.assertEquals(uriuris.get(URI.create("http://example.com/uri.xml")), URI.create("myuri.xml"));
+		Assert.assertEquals(sytemUris.get(URI.create("http://example.com/groupfile.xml")), URI.create("mybase/mysistemIdgroup.xml"));
+		Assert.assertEquals(sytemUris.get(URI.create("http://example.com/file.xml")), URI.create("mysistemId.xml"));
+        }
+        @Test
+        public void testUris(){
 
-	}
+		Map<URI, URI> uris=catalog.getUriMappings();
+		Assert.assertEquals(uris.size(), 3);
+		Assert.assertEquals(uris.get(URI.create("http://example.com/groupuri.xml")), URI.create("mybase/myurigroup.xml"));
+		Assert.assertEquals(uris.get(URI.create("http://example.com/groupuri2.xml")), URI.create("mybase/myurigroup2.xml"));
+		Assert.assertEquals(uris.get(URI.create("http://example.com/uri.xml")), URI.create("myuri.xml"));
+        }
 
+        @Test
+        public void rewrite(){
+
+		Map<URI, URI> uris=catalog.getRewriteUris();
+		Assert.assertEquals(uris.size(), 1);
+		Assert.assertEquals(uris.get(URI.create("http://example.com/static/")), URI.create("mybase/static/"));
+        }
 }
