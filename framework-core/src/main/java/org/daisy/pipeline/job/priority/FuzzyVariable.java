0d8c05e83ddc7b0ea82ee1de2d0d1bca76dec9bc
capitancambio <capitan.cambio@gmail.com>
2014-02-18 16:43:16 +0000

    Refactor fuzzy sets so we set the funciton instead of using inheritance

--- framework-core/src/main/java/org/daisy/pipeline/job/priority/FuzzyVariable.java
+++ framework-core/src/main/java/org/daisy/pipeline/job/priority/FuzzyVariable.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.google.common.base.Function;
+import com.google.common.base.Supplier;
 
 
 /**
@@ -23,104 +24,9 @@ public class FuzzyVariable {
                 return this.sets;
         }
 
-        static class FuzzySet implements Function<Double,Double>{
-                /** A fuzzy set which basically have this shape
-                 *    --------     1
-                 *   /        \
-                 *  /          \
-                 * /            \  0
-                */
-                private double []points;
-                private String name;
-                private double weight;
-
-                private FuzzySet(String name, double weight, double x1, double x2, double x3,double x4){
-                        this.name=name;
-                        this.weight=weight;
-                        this.points=new double[]{x1,x2,x3,x4};
-                }
-                /**
-                 *  Builds a new trapeziod by defining the start, end of the first slope, start of the last slope and the ending point.
-                 *  x1<=x2<=x3<=x4
-                 */
-                public static FuzzySet newSet(String name, double weight, double x1, double x2, double x3,double x4){
-                        if (!(x1<=x2 && x2<=x3 && x3<=x4)){
-                                throw new IllegalArgumentException(String.format("Fuzzy set boundaries are not x1<=x2<=x3<=x4 (%s, %s, %s, %s)", 
-                                                        x1,x2,x3,x4));
-                        }
-
-
-                        return new FuzzySet(name,weight,x1,x2,x3,x4);
-
-                }
-
-                /**
-                 * @return the name
-                 */
-                public String getName() {
-                        return name;
-                }
-
-
-                /**
-                 * @return the weight
-                 */
-                public double getWeight() {
-                        return weight;
-                }
-
-                @Override
-                /**
-                 * Computes the memebership value
-                 */
-                public Double apply(Double x) {
-                        double res=-1;
-                        int seg=getSegment(x);
-                        switch(seg){
-                                case -1: 
-                                        res=0;
-                                        break;
-                                case 0: //(x-x_0)/(x_1-x_0)
-                                        res=(x-this.points[seg])/(this.points[seg+1]-this.points[seg]);
-                                        break;
-                                case 1:
-                                        res=1;
-                                        break;
-                                case 2://(x1-x)/(x_1-x_0)
-                                        res=(this.points[seg+1]-x)/(this.points[seg+1]-this.points[seg]);
-                                        break;
-                        }
-                        return new Double(res);
-                }
-              
-                /**
-                 * Returns the segment index where this vlaue falls into. -1 is out of the bounds
-                 * 
-                 */
-                int getSegment(double x){
-                        if (x<0 || x>1.0){
-                                return -1;
-                        }
-                        for (int  i=0;i<this.points.length-1;i++){
-                                if(inSegment(i,x)){
-                                        return i;
-                                }
-                        }
-                        return -1;
-                                
-                                
-                }
-                /**
-                 *
-                 * Check the segment where x has to be projected into
-                 */
-                boolean inSegment(int segment,double x){
-                        return (x>=this.points[segment] && x<=this.points[segment+1]);
-                }
-
+ 
 
                 
-        }
 }
 
 
--- framework-core/src/test/java/org/daisy/pipeline/job/priority/FuzzyVariableTest.java
+++ framework-core/src/test/java/org/daisy/pipeline/job/priority/FuzzyVariableTest.java
@@ -1,66 +1,18 @@
 package org.daisy.pipeline.job.priority;
 
-import org.daisy.pipeline.job.priority.FuzzyVariable.FuzzySet;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class FuzzyVariableTest {
 
-        @Test 
-        public void newFuzzySet(){
-                String name="Name";
-                double weight=10.0;
-                FuzzySet set=FuzzySet.newSet(name,weight,0.0,0.3,0.4,0.7);
-                Assert.assertEquals(weight,set.getWeight(),0.0);
-                Assert.assertEquals("Check name",name,set.getName());
+        //@Test 
+        //public void newFuzzySet(){
+                //String name="Name";
+                //double weight=10.0;
+                //FuzzySet set=FuzzySet.newSet(name,weight,0.0,0.3,0.4,0.7);
+                //Assert.assertEquals(weight,set.getWeight(),0.0);
+                //Assert.assertEquals("Check name",name,set.getName());
 
-        }
+        //}
 
-        @Test(expected=IllegalArgumentException.class)
-        public void newBadBoundariesOrder(){
-                FuzzySet set=FuzzySet.newSet("",0.0,0.5,0.3,0.4,0.7);
-        }
-
-        @Test
-        public void inSegmentOut(){
-                FuzzySet set=FuzzySet.newSet("",0.0,0.2,0.6,0.7,0.8);
-                Assert.assertEquals("Out of lower boundary 0.1",-1,set.getSegment(0.1));
-                Assert.assertEquals("Out of upper boundary 0.9",-1,set.getSegment(0.9));
-        }
-        @Test
-        public void applyOutOfSet(){
-                FuzzySet set=FuzzySet.newSet("",0.0,0.2,0.6,0.7,0.8);
-                Assert.assertEquals("membership 0, out of lower ",0.0,set.apply(0.1),0.0);
-                Assert.assertEquals("memebership 0, out of upper0.9",0.0,set.apply(0.9),0.0);
-        }
-
-
-        @Test
-        public void inSegment(){
-                FuzzySet set=FuzzySet.newSet("",0.0,0.2,0.6,0.7,0.8);
-                Assert.assertEquals("seg 0",0,set.getSegment(0.3));
-                Assert.assertEquals("seg 1",1,set.getSegment(0.65));
-                Assert.assertEquals("seg 2",2,set.getSegment(0.75));
-        }
-
-        @Test
-        public void applyFirstSeg(){
-                FuzzySet set=FuzzySet.newSet("",0.0,0.2,0.6,0.7,0.8);
-                //(x-0.2)/(0.6-0.2)=0.25
-                Assert.assertEquals("Slope",0.25,set.apply(0.3),0.01);
-        }
-
-        @Test
-        public void applySecondSeg(){
-                FuzzySet set=FuzzySet.newSet("",0.0,0.2,0.6,0.7,0.8);
-                //(x-0.2)/(0.6-0.2)=0.25
-                Assert.assertEquals("Slope",1.0,set.apply(0.65),0.0);
-        }
-
-        @Test
-        public void applyThirdSeg(){
-                FuzzySet set=FuzzySet.newSet("",0.0,0.2,0.6,0.7,0.8);
-                //(0.8-0.72)/(0.8-0.7)=0.25
-                Assert.assertEquals("Slope",0.8,set.apply(0.72),0.0);
-        }
 }
