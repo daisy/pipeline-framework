58eac3edbdc6082f5268cb0bec47ce94744d0ff9
capitancambio <capitan.cambio@gmail.com>
2014-03-03 11:46:43 +0000

    Modify ClientStorage to base all the interactions on ids

--- framework-persistence/src/main/java/org/daisy/pipeline/persistence/webservice/PersistentClientStorage.java
+++ framework-persistence/src/main/java/org/daisy/pipeline/persistence/webservice/PersistentClientStorage.java
@@ -13,99 +13,111 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 
 public class PersistentClientStorage implements ClientStorage {
 
-    private static Logger logger = LoggerFactory
-            .getLogger(PersistentClientStorage.class);
+        private static Logger logger = LoggerFactory
+                .getLogger(PersistentClientStorage.class);
 
-    //This client is the default client for "non client-aware" uses of the api
-    final static PersistentClient DEFAULT = new PersistentClient(
-            "DEFAULT_PERSISTENT_CLIENT_1685216", "", Role.ADMIN, "",
-            Priority.MEDIUM);
+        //This client is the default client for "non client-aware" uses of the api
+        final static PersistentClient DEFAULT = new PersistentClient(
+                        "DEFAULT_PERSISTENT_CLIENT_1685216", "", Role.ADMIN, "",
+                        Priority.MEDIUM);
 
-    private Database database;
+        private Database database;
 
-    public PersistentClientStorage(Database database) {
-        this.database = database;
-    }
-
-    public void setDatabase(Database database) {
-        this.database = database;
-    }
-
-    @Override
-    public List<? extends Client> getAll() {
-        return database.runQuery(String.format("select c from PersistentClient as c where c.id <>'%s'",DEFAULT.getId()),
-                PersistentClient.class);
-    }
-
-    @Override
-    public Optional<Client> get(String id) {
-        String q = String.format(
-                "select c from PersistentClient as c where c.id='%s'", id);
-        try {
-            return Optional.of((Client)database.getFirst(q, PersistentClient.class));
-        } catch (NoResultException e) {
-            return Optional.absent();
+        public PersistentClientStorage(Database database) {
+                this.database = database;
         }
-    }
 
-    @Override
-    public boolean delete(Client client) {
-        Optional<Client>clientInDb = get(client.getId());
-        if (!clientInDb.isPresent()) {
-            return false;
+        public void setDatabase(Database database) {
+                this.database = database;
         }
-        return database.deleteObject(clientInDb.get());
-    }
 
-    @Override
-    public Optional<Client> update(Client client,String secret, Role role,
-            String contactInfo, Priority priority) {
-        if(client.getId().equals(DEFAULT.getId())){
-            return Optional.absent();
+        @Override
+        public List<? extends Client> getAll() {
+                //ignore default
+                return database.runQuery(String.format("select c from PersistentClient as c where c.id <>'%s'",DEFAULT.getId()),
+                                PersistentClient.class);
         }
-        Optional<Client>clientInDb = get(client.getId());
-        if (clientInDb.isPresent()) {
-            PersistentClient pClient=(PersistentClient)clientInDb.get(); 
-            pClient.setContactInfo(contactInfo);
-            pClient.setRole(role);
-            pClient.setSecret(secret);
-            database.updateObject(pClient);
-            clientInDb=Optional.of((Client)pClient);
-        }
-        return clientInDb;
-    }
 
-    @Override
-    public Optional<Client> addClient(String id, String secret, Role role,
-            String contactInfo, Priority priority) {
-        Optional<Client>client = get(id);
-        if (client.isPresent()) {
-            logger.error(String.format("ID %s is already in use.", id));
-            return Optional.absent();
+        @Override
+        public Optional<Client> get(String id) {
+                //get null?
+                String q = String.format(
+                                "select c from PersistentClient as c where c.id='%s'", id);
+                try {
+                        return Optional.of((Client)database.getFirst(q, PersistentClient.class));
+                } catch (NoResultException e) {
+                        return Optional.absent();
+                }
         }
-        PersistentClient newClient= new PersistentClient(id, secret, role, contactInfo, priority);
-        database.addObject(newClient);
-        return Optional.of((Client)newClient);
-    }
 
-    @Override
-    public Optional<Client> addClient(String id, String secret, Role role,
-            String contactInfo) {
-        return addClient(id, secret, role, contactInfo, Priority.MEDIUM);
-    }
-
-    @Override
-    public Client defaultClient() {
-        //try and get the client from the db
-        Optional<Client> def = this.get(DEFAULT.getId());
-        if (!def.isPresent()) {
-            this.database.addObject(DEFAULT);
+        @Override
+        public boolean delete(String id) {
+                //check if it's default
+                if(DEFAULT.getId().equals(id)){
+                        return false;
+                }
+                Optional<Client>clientInDb = get(id);
+                if (!clientInDb.isPresent()) {
+                        return false;
+                }
+                return database.deleteObject(clientInDb.get());
         }
-        return DEFAULT;
-    }
+
+        @Override
+        public Optional<Client> update(String id,String secret, Role role,
+                        String contactInfo, Priority priority) {
+                Optional<Client> optClient=this.get(id);
+                //no such client
+                if(!optClient.isPresent()){
+                        return optClient;
+                }
+
+
+                if(optClient.get().getId().equals(DEFAULT.getId())){
+                        return Optional.absent();
+                }
+                PersistentClient pClient=(PersistentClient)optClient.get(); 
+                pClient.setSecret(secret);
+                pClient.setRole(role);
+                pClient.setContactInfo(contactInfo);
+                pClient.setPriority(priority);
+                database.updateObject(pClient);
+                return Optional.of((Client)pClient);
+        }
+
+        @Override
+        public Optional<Client> addClient(String id, String secret, Role role,
+                        String contactInfo, Priority priority) {
+                Preconditions.checkArgument(id!=null,"Client id can't be null");
+                Optional<Client>client = get(id);
+                if (client.isPresent()) {
+                        logger.error(String.format("ID %s is already in use.", id));
+                        return Optional.absent();
+                }
+                PersistentClient newClient= new PersistentClient(id, secret, role, contactInfo, priority);
+                database.addObject(newClient);
+                return Optional.of((Client)newClient);
+        }
+
+        @Override
+        public Optional<Client> addClient(String id, String secret, Role role,
+                        String contactInfo) {
+                return addClient(id, secret, role, contactInfo, Priority.MEDIUM);
+        }
+
+        @Override
+        public Client defaultClient() {
+                //try and get the client from the db
+                Optional<Client> def = this.get(DEFAULT.getId());
+                if (!def.isPresent()) {
+                        this.database.addObject(DEFAULT);
+                }
+                return DEFAULT;
+        }
 
 
 }
