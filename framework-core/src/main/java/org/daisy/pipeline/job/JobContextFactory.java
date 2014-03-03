583e8f9e2bf6a8bfc539a6d2cd5032f45f667bf8
capitancambio <capitan.cambio@gmail.com>
2014-03-03 17:16:03 +0000

    Add new factory method for JobContexts using a boolean flag

--- framework-core/src/main/java/org/daisy/pipeline/job/JobContextFactory.java
+++ framework-core/src/main/java/org/daisy/pipeline/job/JobContextFactory.java
@@ -16,79 +16,59 @@ import com.google.common.eventbus.EventBus;
  *
  *
  */
-public final class JobContextFactory {
+public class JobContextFactory {
 
-	private static final Logger logger = LoggerFactory.getLogger(JobContextFactory.class);
+        private static final Logger logger = LoggerFactory.getLogger(JobContextFactory.class);
 
-	private JobMonitorFactory monitorFactory;
-        //TODO: I bet that this shouldn't be here
-        //check the implications with the osgi service
-	private EventBusProvider eventbusProvider;
+        private JobMonitorFactory monitorFactory;
+
+        private Client client;
 
 
-	public JobContextFactory() {
-		//nothing
-	}
+        public JobContextFactory(JobMonitorFactory monitorFactory,Client client) {
+                this.monitorFactory=monitorFactory;
+                this.client=client;
+        }
 
-	public JobContext newMappingJobContext(Client client,String niceName,BoundXProcScript boundScript,ResourceCollection collection){
-		JobId id = JobIdFactory.newId();
-		AbstractJobContext ctxt=null;
-		try{
-			 ctxt=new MappingJobContext(client,id,niceName,boundScript,collection);
-		}catch (IOException ex){
-			throw new RuntimeException("Error while creating MappingJobContext",ex);
-		}
-		this.configure(ctxt);
-		return ctxt;
+         
+        public JobContext newJobContext(boolean mapping,String niceName,BoundXProcScript boundScript,ResourceCollection collection){
+                //if mapping create a new mapping context
+                if(mapping){
+                        return this.newMappingJobContext(niceName,boundScript,collection);
+                }else{
+                //otherwise create a simple one
+                        return this.newJobContext(niceName,boundScript);
+                }
+        }
 
-	}
+        public JobContext newMappingJobContext(String niceName,BoundXProcScript boundScript,ResourceCollection collection){
+                JobId id = JobIdFactory.newId();
+                AbstractJobContext ctxt=null;
+                try{
+                         ctxt=new MappingJobContext(client,id,niceName,boundScript,collection);
+                }catch (IOException ex){
+                        throw new RuntimeException("Error while creating MappingJobContext",ex);
+                }
+                this.configure(ctxt);
+                return ctxt;
 
-	public JobContext newMappingJobContext(Client client,String niceName,BoundXProcScript boundScript){
-		return newMappingJobContext(client,niceName,boundScript,null);
-	}
+        }
 
-	public JobContext newJobContext(Client client,String niceName,BoundXProcScript boundScript){
-		JobId id = JobIdFactory.newId();
-		AbstractJobContext ctxt=new SimpleJobContext(client,id,niceName,boundScript);
-		this.configure(ctxt);
-		return ctxt;
+        public JobContext newMappingJobContext(String niceName,BoundXProcScript boundScript){
+                return newMappingJobContext(niceName,boundScript,null);
+        }
 
-	}
-	public void setJobMonitorFactory(JobMonitorFactory monitorFactory){
-			logger.debug("setting monitor factory");
-			this.monitorFactory=monitorFactory;
-		
-	}
-	public void setEventBusProvider(EventBusProvider eventbusProvider){
-			logger.debug("setting even bus factory");
-			this.eventbusProvider=eventbusProvider;
-	}
+        public JobContext newJobContext(String niceName,BoundXProcScript boundScript){
+                JobId id = JobIdFactory.newId();
+                AbstractJobContext ctxt=new SimpleJobContext(this.client,id,niceName,boundScript);
+                this.configure(ctxt);
+                return ctxt;
 
-	public EventBus getEventBus(){
-		return this.eventbusProvider.get();
-	}
-		
-	//FIXME: probably move these two methods somewhere else, maybe a dummy class for the framework just tu publish this.
-	public void setPropertyPublisherFactory(PropertyPublisherFactory propertyPublisherFactory){
-		PropertyPublisher propertyPublisher=propertyPublisherFactory.newPropertyPublisher();	
-		//the property publishing step goes here
-		propertyPublisher.publish("org.daisy.pipeline.iobase" ,System.getProperty("org.daisy.pipeline.iobase","" ),this.getClass());
-		propertyPublisher.publish("org.daisy.pipeline.home" ,System.getProperty("org.daisy.pipeline.home","" ),this.getClass());
-		propertyPublisher.publish("org.daisy.pipeline.logdir",System.getProperty("org.daisy.pipeline.logdir","" ),this.getClass());
-	}
+        }
 
-	public void unsetPropertyPublisherFactory(PropertyPublisherFactory propertyPublisherFactory){
-		PropertyPublisher propertyPublisher=propertyPublisherFactory.newPropertyPublisher();	
-		//the property unpublishing step goes here
-		propertyPublisher.unpublish("org.daisy.pipeline.iobase" ,  this.getClass());
-		propertyPublisher.unpublish("org.daisy.pipeline.home"   ,  this.getClass());
-		propertyPublisher.unpublish("org.daisy.pipeline.logdir" ,  this.getClass());
-
-	}
-
-	public void configure(RuntimeConfigurable runtimeObj){
-		logger.debug(String.format("configuring object %s",runtimeObj));
-		runtimeObj.setMonitorFactory(this.monitorFactory);
-	}
+        public void configure(RuntimeConfigurable runtimeObj){
+                logger.debug(String.format("configuring object %s",runtimeObj));
+                runtimeObj.setMonitorFactory(this.monitorFactory);
+        }
 
 }
