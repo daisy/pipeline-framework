32ccd9623a995d7fbaacc9949f93976a794a5558
capitancambio <capitan.cambio@gmail.com>
2014-03-06 13:29:19 +0000

    Modify VolatileJobStorage to filter by client

--- framework-volatile/src/test/java/org/daisy/pipeline/nonpersistent/jobs/Mock.java
+++ framework-volatile/src/test/java/org/daisy/pipeline/nonpersistent/jobs/Mock.java
@@ -1,29 +1,27 @@
 package org.daisy.pipeline.nonpersistent.jobs;
 
-import org.daisy.pipeline.clients.Client.Role;
+import org.daisy.pipeline.clients.Client;
 import org.daisy.pipeline.job.AbstractJobContext;
 import org.daisy.pipeline.job.JobContext;
 import org.daisy.pipeline.job.JobId;
 import org.daisy.pipeline.job.JobIdFactory;
 import org.daisy.pipeline.job.URIMapper;
-import org.daisy.pipeline.job.priority.Priority;
-import org.daisy.pipeline.nonpersistent.webservice.VolatileClient;
 import org.daisy.pipeline.script.BoundXProcScript;
 
 public class Mock {
 
 	static class MockedJobContext extends AbstractJobContext {
 
-		public MockedJobContext(JobId id, String niceName,
+		public MockedJobContext(Client cl,JobId id, String niceName,
 				BoundXProcScript boundScript, URIMapper mapper) {
-			super(new VolatileClient("","",Role.ADMIN,"",Priority.MEDIUM),id, niceName, boundScript, mapper);
+			super(cl,id, niceName, boundScript, mapper);
 			// TODO Auto-generated constructor stub
 		}
 	}
 
 
-	public static JobContext newJobContext(){
-		return new MockedJobContext(JobIdFactory.newId(),null,null,null);
+	public static JobContext newJobContext(Client cl){
+		return new MockedJobContext(cl,JobIdFactory.newId(),null,null,null);
 	}
 	
 }
