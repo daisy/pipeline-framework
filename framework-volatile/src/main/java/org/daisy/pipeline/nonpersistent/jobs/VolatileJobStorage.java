1cb5f42387afa944b65725688eab0f6d5455398e
capitancambio <capitan.cambio@gmail.com>
2014-03-06 13:29:19 +0000

    Modify VolatileJobStorage to filter by client

--- framework-volatile/src/main/java/org/daisy/pipeline/nonpersistent/jobs/VolatileJobStorage.java
+++ framework-volatile/src/main/java/org/daisy/pipeline/nonpersistent/jobs/VolatileJobStorage.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.daisy.pipeline.clients.Client;
+import org.daisy.pipeline.clients.Client.Role;
 import org.daisy.pipeline.event.EventBusProvider;
 import org.daisy.pipeline.job.Job;
 import org.daisy.pipeline.job.JobContext;
@@ -14,52 +16,97 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Collections2;
 import com.google.common.eventbus.EventBus;
 
 public final class VolatileJobStorage implements JobStorage {
-	private static final Logger logger = LoggerFactory.getLogger(VolatileJobStorage.class);
+        private static final Logger logger = LoggerFactory
+                        .getLogger(VolatileJobStorage.class);
 
-	private Map<JobId,Job> jobs = Collections.synchronizedMap(new HashMap<JobId,Job>()); 
-	private EventBus bus;
+        private Map<JobId, Job> jobs = Collections
+                        .synchronizedMap(new HashMap<JobId, Job>());
+        private EventBus bus;
+
+        private Predicate<Job> filter = Predicates.alwaysTrue();
+
+        public VolatileJobStorage(){}
 
 
-	public void setEventBusProvider(EventBusProvider busProvider){
-		this.bus=busProvider.get();
-	}
 
-	@Override
-	public Iterator<Job> iterator() {
-		return this.jobs.values().iterator();
-	}
+        /**
+         * @param bus
+         * @param filter
+         */
+        public VolatileJobStorage(Map<JobId, Job> jobs,EventBus bus,
+                        Predicate<Job> filter) {
+                this.jobs=jobs;
+                this.bus = bus;
+                this.filter = filter;
+        }
 
-	@Override
-	public synchronized Job add(final JobContext ctxt) {
-			if(!this.jobs.containsKey(ctxt.getId())){
-				//Store the job before its status gets broadcasted
-				Job job= new Job.JobBuilder()
-					.withEventBus(this.bus)
-					.withContext(ctxt)
-					.build(new Function<Job, Job>() {
-						@Override
-						public Job apply(Job job) {
-							VolatileJobStorage.this.jobs.put(ctxt.getId(),job);
-							return job;
-						}
-					});
-				return job;
-			}
-		return null;
-	}
+        public void setEventBusProvider(EventBusProvider busProvider) {
+                this.bus = busProvider.get();
+        }
 
-	@Override
-	public synchronized Job remove(JobId jobId) {
-		return this.jobs.remove(jobId);
-		
-	}
+        @Override
+        public Iterator<Job> iterator() {
+                return Collections2.filter(this.jobs.values(),this.filter).iterator();
+        }
 
-	@Override
-	public synchronized Job get(JobId jobId) {
-		return this.jobs.get(jobId);
-	}
-	
+        @Override
+        public synchronized Optional<Job> add(final JobContext ctxt) {
+
+                if (!this.jobs.containsKey(ctxt.getId())) {
+                        //Store the job before its status gets broadcasted
+                        Job job = new Job.JobBuilder().withEventBus(this.bus)
+                                        .withContext(ctxt).build(new Function<Job, Job>() {
+                                                @Override
+                                                public Job apply(Job job) {
+                                                        VolatileJobStorage.this.jobs.put(ctxt.getId(), job);
+                                                        return job;
+                                                }
+                                        });
+                        return Optional.of(job);
+                }
+                return Optional.absent();
+        }
+
+        @Override
+        public synchronized Optional<Job> remove(JobId jobId) {
+                Optional<Job> job = this.get(jobId);
+                if(job.isPresent()){
+                        this.jobs.remove(jobId);
+                }
+                return job;
+
+        }
+
+        @Override
+        public synchronized Optional<Job> get(JobId jobId) {
+                Job job=this.jobs.get(jobId);
+                if(this.filter.apply(job)){
+                        return Optional.fromNullable(job);
+                }
+                return Optional.absent();
+        }
+
+        @Override
+        public JobStorage filterBy(final Client client) {
+                if (client.getRole().equals(Role.ADMIN)){
+                        return this;
+                }else{
+                        return new VolatileJobStorage(this.jobs,this.bus,new Predicate<Job>(){
+
+                                @Override
+                                public boolean apply(Job job) {
+                                        //check if the client id is the one we're filtering by
+                                        return job.getContext().getClient().getId().equals(client.getId());
+                                }
+                        });
+                }
+        }
+        
 }
