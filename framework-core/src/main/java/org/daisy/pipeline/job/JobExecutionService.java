a364dd2ff8ec47bcdd9beb8d405284107adf2292
capitancambio <capitan.cambio@gmail.com>
2014-04-08 10:53:12 +0100

    Set job updating frequency to 1

--- framework-core/src/main/java/org/daisy/pipeline/job/JobExecutionService.java
+++ framework-core/src/main/java/org/daisy/pipeline/job/JobExecutionService.java
@@ -18,7 +18,7 @@ public interface JobExecutionService {
 	public void submit(Job job);
 
         public ExecutionQueue getExecutionQueue();
-
+        //TODO: merge this filter with the getQueue
         public JobExecutionService filterBy(Client client);
 
 
