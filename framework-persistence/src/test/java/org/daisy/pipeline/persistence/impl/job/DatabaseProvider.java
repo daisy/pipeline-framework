c85fc5b6d3e250f1b25ff2610c267ce447481b8c
Romain Deltour <rdeltour@gmail.com>
2014-04-17 15:45:07 +0200

    Reorganize packages in framework-persistence

---
+++ framework-persistence/src/test/java/org/daisy/pipeline/persistence/impl/job/DatabaseProvider.java
@@ -0,0 +1,17 @@
+package org.daisy.pipeline.persistence.impl.job;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.daisy.pipeline.persistence.impl.Database;
+
+public class DatabaseProvider {
+	private static EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("pipeline-pu-test");
+
+	public static Database getDatabase(){
+		Database db= new Database();
+		db.setEntityManagerFactory(entityManagerFactory);
+		return db;
+	}
+
+}
