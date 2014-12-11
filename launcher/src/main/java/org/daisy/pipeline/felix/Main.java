7811e89fa658d8db1dc62c811530f75b86dc1bdd
capitancambio <capitan.cambio@gmail.com>
2014-12-11 08:56:50 +0000

    Add all the needed services to the example

--- launcher/src/main/java/org/daisy/pipeline/felix/Main.java
+++ launcher/src/main/java/org/daisy/pipeline/felix/Main.java
@@ -1,15 +1,11 @@
 package org.daisy.pipeline.felix;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.concurrent.Future;
 
-import org.apache.felix.framework.FrameworkFactory;
+import org.daisy.pipeline.event.EventBusProvider;
 import org.daisy.pipeline.job.JobManagerFactory;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.launch.Framework;
-import org.osgi.util.tracker.ServiceTracker;
+import org.daisy.pipeline.script.ScriptRegistry;
+import org.daisy.pipeline.webserviceutils.storage.WebserviceStorage;
 
 /**
  * Main that waits for a Runnable service to arrive it can be run in the main thread.
@@ -18,106 +14,30 @@ import org.osgi.util.tracker.ServiceTracker;
  *
  * The methods waitForSWT just waits for a runnable service and executes it
  */
-public class Main extends org.apache.felix.main.Main{
-        /**
-         * Switch for specifying bundle directory.
-         **/
-        public static final String BUNDLE_DIR_SWITCH = "-b";
-        /**
-         * The property name used to specify whether the launcher should
-         * install a shutdown hook.
-         **/
-        public static final String SHUTDOWN_HOOK_PROP = "felix.shutdown.hook";
-        /**
-         * The property name used to specify an URL to the system
-         * property file.
-         **/
-        public static final String SYSTEM_PROPERTIES_PROP = "felix.system.properties";
-        /**
-         * The default name used for the system properties file.
-         **/
-        public static final String SYSTEM_PROPERTIES_FILE_VALUE = "system.properties";
-        /**
-         * The property name used to specify an URL to the configuration
-         * property file to be used for the created the framework instance.
-         **/
-        public static final String CONFIG_PROPERTIES_PROP = "felix.config.properties";
-        /**
-         * The default name used for the configuration properties file.
-         **/
-        public static final String CONFIG_PROPERTIES_FILE_VALUE = "config.properties";
-        /**
-         * Name of the configuration directory.
-         */
-        public static final String CONFIG_DIRECTORY = "conf";
+public class Main {
 
         public static void main(String[] args) throws Exception {
                 System.out.println("In main");
                 PipelineFramework pf=new PipelineFramework();
                 pf.init();
-                System.out.println("Getting the futurible");
-                Future<JobManagerFactory> future= pf.getService(JobManagerFactory.class);
-                System.out.println("Calling get...");
-                future.get();
-                System.out.println("I got the service!");
+                System.out.println("Getting the futuribles");
+                Future<JobManagerFactory> jManFactorySupplier= pf.getService(JobManagerFactory.class);
+                Future<EventBusProvider> eventBusProviderSuppiler=pf.getService(EventBusProvider.class);
+                Future<WebserviceStorage> webServiceStorageSuppiler=pf.getService(WebserviceStorage.class);
+                Future<ScriptRegistry> scriptRegistrySuppiler=pf.getService(ScriptRegistry.class);
+                System.out.println("Doing time comsuming things....");
+                System.out.println("Calling gets...");
+                jManFactorySupplier.get();
+                System.out.println("job manager factory");
+                eventBusProviderSuppiler.get();
+                System.out.println("event bus supplier");
+                webServiceStorageSuppiler.get();
+                System.out.println("web service storage");
+                scriptRegistrySuppiler.get();
+                System.out.println("script registry");
+                System.out.println("I got the services!");
         }
 
-        /**
-         * Wait for a Runnable service to arrive and execute it
-         */
-        private static void waitForSWT(Framework fwk) {
-                BundleContext bCtxt = fwk.getBundleContext();
-                ServiceTracker<Runnable,Runnable> tracker = new ServiceTracker<Runnable,Runnable>(
-                                bCtxt, Runnable.class, null);
-                Runnable runnable;
-                try {
-                        tracker.open();
-                        runnable = (Runnable)tracker.waitForService(0L);
-                        if(runnable!=null){
-                                runnable.run();
-                        }else{
-                                System.err.println("Runnable was null: doing nothing");
-                        }
-                        tracker.close();
-                } catch (Exception e) {
-                        System.err.println("Exection in waitForSWT"+e.getMessage());
-                        e.printStackTrace();
-                }
 
-        }
-
-        /**
-         * Simple method to parse META-INF/services file for framework factory.
-         * Currently, it assumes the first non-commented line is the class name
-         * of the framework factory implementation.
-         * @return The created <tt>FrameworkFactory</tt> instance.
-         * @throws Exception if any errors occur.
-         **/
-        static FrameworkFactory getFrameworkFactory() throws Exception
-        {
-                URL url = Main.class.getClassLoader().getResource(
-                                "META-INF/services/org.osgi.framework.launch.FrameworkFactory");
-                if (url != null)
-                {
-                        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-                        try
-                        {
-                                for (String s = br.readLine(); s != null; s = br.readLine())
-                                {
-                                        s = s.trim();
-                                        // Try to load first non-empty, non-commented line.
-                                        if ((s.length() > 0) && (s.charAt(0) != '#'))
-                                        {
-                                                return (FrameworkFactory) Class.forName(s).newInstance();
-                                        }
-                                }
-                        }
-                        finally
-                        {
-                                if (br != null) br.close();
-                        }
-                }
-                throw new Exception("Could not find framework factory.");
-        }
 } 
 
--- launcher/src/main/java/org/daisy/pipeline/felix/PipelineFramework.java
+++ launcher/src/main/java/org/daisy/pipeline/felix/PipelineFramework.java
@@ -1,5 +1,8 @@
 package org.daisy.pipeline.felix;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -15,7 +18,11 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.launch.Framework;
 import org.osgi.util.tracker.ServiceTracker;
 
-public class PipelineFramework {
+public class PipelineFramework extends org.apache.felix.main.Main{
+        /**
+         * The default name used for the configuration properties file.
+         **/
+        public static final String CONFIG_PROPERTIES_FILE_VALUE = "config.properties";
 
         private Framework fwk = null;
         private ExecutorService executor= new ThreadPoolExecutor(3, 3, 10, TimeUnit.HOURS,new LinkedBlockingQueue<Runnable>());
@@ -25,21 +32,21 @@ public class PipelineFramework {
                 // We support at most one argument, which is the bundle
                 // cache directory.
                 // Load system properties.
-                Main.loadSystemProperties();
+                PipelineFramework.loadSystemProperties();
                 // Read configuration properties.
-                Map<String, String> configProps = Main.loadConfigProperties();
+                Map<String, String> configProps = PipelineFramework.loadConfigProperties();
                 // If no configuration properties were found, then create
                 // an empty properties object.
                 if (configProps == null) {
                         System.err
-                                        .println("No " + Main.CONFIG_PROPERTIES_FILE_VALUE + " found.");
+                                        .println("No " + PipelineFramework.CONFIG_PROPERTIES_FILE_VALUE + " found.");
                         configProps = new HashMap<String, String>();
                 }
                 // Copy framework properties from the system properties.
-                Main.copySystemProperties(configProps);
+                PipelineFramework.copySystemProperties(configProps);
                 try {
                         // Create an instance of the framework.
-                        FrameworkFactory factory = Main.getFrameworkFactory();
+                        FrameworkFactory factory = PipelineFramework.getFrameworkFactory();
                         fwk = factory.newFramework(configProps);
                         // Initialize the framework, but don't start it yet.
                         fwk.init();
@@ -80,6 +87,39 @@ public class PipelineFramework {
                 Future<T> future=this.executor.submit(callable);
                 return future;
 
+        }
+        /**
+         * Simple method to parse META-INF/services file for framework factory.
+         * Currently, it assumes the first non-commented line is the class name
+         * of the framework factory implementation.
+         * @return The created <tt>FrameworkFactory</tt> instance.
+         * @throws Exception if any errors occur.
+         **/
+        static FrameworkFactory getFrameworkFactory() throws Exception
+        {
+                URL url = FrameworkFactory.class.getClassLoader().getResource(
+                                "META-INF/services/org.osgi.framework.launch.FrameworkFactory");
+                if (url != null)
+                {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+                        try
+                        {
+                                for (String s = br.readLine(); s != null; s = br.readLine())
+                                {
+                                        s = s.trim();
+                                        // Try to load first non-empty, non-commented line.
+                                        if ((s.length() > 0) && (s.charAt(0) != '#'))
+                                        {
+                                                return (FrameworkFactory) Class.forName(s).newInstance();
+                                        }
+                                }
+                        }
+                        finally
+                        {
+                                if (br != null) br.close();
+                        }
+                }
+                throw new Exception("Could not find framework factory.");
         }
 
 }
