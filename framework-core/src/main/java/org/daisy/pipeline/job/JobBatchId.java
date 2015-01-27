9f678d8b486edb51b9d6c7c4eaacff1fcbf846fa
capitancambio <capitan.cambio@gmail.com>
2015-01-27 12:15:49 +0000

    Change batch id to be a glorified string instead of uuid

--- framework-core/src/main/java/org/daisy/pipeline/job/JobBatchId.java
+++ framework-core/src/main/java/org/daisy/pipeline/job/JobBatchId.java
@@ -2,7 +2,22 @@ package org.daisy.pipeline.job;
 
 import java.io.Serializable;
 
-public interface JobBatchId extends Serializable{
+public class JobBatchId {
 
+
+        private String id;
+        /**
+         *
+         */
+        public JobBatchId(String id) {
+                this.id=id;
+        }
+
+        /**
+         * @return the id
+         */
+        public String toString() {
+                return id;
+        }
         
 }
