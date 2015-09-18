24df8418fd0cae509a12abdac1db5683246470ef
Romain Deltour <rdeltour@gmail.com>
2015-09-18 15:18:10 +0200

    Make Index.java immutability

--- framework-core/src/main/java/org/daisy/pipeline/job/Index.java
+++ framework-core/src/main/java/org/daisy/pipeline/job/Index.java
@@ -5,28 +5,31 @@ package org.daisy.pipeline.job;
  *
  *
  */
-public class Index {
-        private String index;
+public final class Index {
+	private final String index;
 
-        /**
-         * Creates an index based on a string
-         * @param index
-         */
-        public Index(String index) {
-                this.index = index;
-        }
+	/**
+	 * Creates an index based on a string
+	 * 
+	 * @param index
+	 */
+	public Index(String index) {
+		if (index == null)
+			throw new NullPointerException();
+		this.index = index;
+	}
 
-        public Index stripPrefix() {
-                int idx=this.index.indexOf('/');
-                if (idx!=0){
-                        return new Index(this.index.substring(idx+1));
-                }
-                return this;
-        }
+	public Index stripPrefix() {
+		int idx = this.index.indexOf('/');
+		if (idx != 0) {
+			return new Index(this.index.substring(idx + 1));
+		}
+		return this;
+	}
 
-        @Override
-        public String toString() {
-                return this.index;
-        }
+	@Override
+	public String toString() {
+		return this.index;
+	}
 
 }
