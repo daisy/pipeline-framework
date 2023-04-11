daisy/pipeline-framework

commit a1ff65fa1d2150c4ba329c60927a2ff219ea3aaa

Clarify contract of XMLTransformer and XProcStep.raiseError()

calabash-adapter/src/main/java/org/daisy/common/xproc/calabash/XProcStep.java
@@ -188,10 +188,15 @@ public interface XProcStep extends com.xmlcalabash.core.XProcStep, XMLTransforme
 						: new XProcException(step, e.getMessage())
 					: cause;
 			} else {
-				throw new XProcException(step,
-				                         "Unexpected error in " + step.getType(),
-				                         XProcException.fromException(e)
-				                                       .rebase(step.getLocation(), stackTrace));
+				/* If the exception is a TransformerException we assume it comes from the {@link
+				 * XMLTransformer.transform()} method used to implement the step. An
+				 * IllegalArgumentException should not happen (indicates a bug in the XProc
+				 * step). Because no other exceptions are mentioned in the contract of this method
+				 * we treat them as unexpected exceptions as well. */
+				return XProcException(step,
+				                      "Unexpected error in " + step.getType(),
+				                      XProcException.fromException(e)
+				                                    .rebase(step.getLocation(), stackTrace));
 			}
 		}
 	}

common-utils/src/main/java/org/daisy/common/transform/XMLTransformer.java
@@ -30,6 +30,8 @@ public interface XMLTransformer {
 	 *         threads, the transformations must be executed in the correct order. {@link
 	 *         Runnable#run()} may throw a {@link TransformerException} if an input could not be
 	 *         read, the transformation failed, or an output could not be written.
+	 * @throws IllegalArgumentException if an invalid input or output value was provided or if an
+	 *                                  input or output value is missing.
 	 */
 	public Runnable transform(Map<QName,InputValue<?>> input, Map<QName,OutputValue<?>> output);
 

commit d93f40f5ad82c264bf89a6294c0dc6c967ec7f2f

Fix compilation error
introduced by commit a1ff65fa1d

calabash-adapter/src/main/java/org/daisy/common/xproc/calabash/XProcStep.java
@@ -193,10 +193,10 @@ public interface XProcStep extends com.xmlcalabash.core.XProcStep, XMLTransforme
 				 * IllegalArgumentException should not happen (indicates a bug in the XProc
 				 * step). Because no other exceptions are mentioned in the contract of this method
 				 * we treat them as unexpected exceptions as well. */
-				return XProcException(step,
-				                      "Unexpected error in " + step.getType(),
-				                      XProcException.fromException(e)
-				                                    .rebase(step.getLocation(), stackTrace));
+				return new XProcException(step,
+				                          "Unexpected error in " + step.getType(),
+				                          XProcException.fromException(e)
+				                                        .rebase(step.getLocation(), stackTrace));
 			}
 		}
 	}

framework-core/src/main/java/org/daisy/pipeline/job/AbstractJob.java
@@ -1,6 +1,5 @@
 package org.daisy.pipeline.job;
 
-import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 import java.util.function.Consumer;